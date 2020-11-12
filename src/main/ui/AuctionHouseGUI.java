e2bba5dc037f7b1b9d3d979495ab39f070191f7a
Jason <ngynjsn@students.cs.ubc.ca>
2020-11-12 02:36:25 -0800

    In middle of phase 3. Added some GUI components. Functional buttons are included.

--- src/main/ui/AuctionHouseGUI.java
+++ src/main/ui/AuctionHouseGUI.java
@@ -7,9 +7,11 @@ import persistence.JsonWriter;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
 
-public class AuctionHouseGUI extends JFrame {
+public class AuctionHouseGUI extends JFrame implements ActionListener {
     private static final String JSON_STORE = "./data/auctioninglist.json";
     private AuctioningList auctioningList;
     private Item currentItem;
@@ -37,24 +39,43 @@ public class AuctionHouseGUI extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         initButtons();
+        initJsonButtons();
         initPanels();
         storePanels();
 
         add(mainPanel);
         pack();
         setVisible(true);
+        initJson();
+    }
 
+    private void initJson() {
         jsonWriter = new JsonWriter(JSON_STORE);
         jsonReader = new JsonReader(JSON_STORE);
     }
 
     private void initButtons() {
         addButton = new JButton("Add an item");
+        addButton.setActionCommand("add");
+        addButton.addActionListener(this);
         removeButton = new JButton("Remove an item");
+        removeButton.setActionCommand("remove");
+        removeButton.addActionListener(this);
         viewButton = new JButton("View your items");
-        saveButton = new JButton("Save your items");
-        loadButton = new JButton("Load previously saved items");
+        viewButton.setActionCommand("view");
+        viewButton.addActionListener(this);
         runButton = new JButton("Run auction house");
+        runButton.setActionCommand("run");
+        runButton.addActionListener(this);
+    }
+
+    private void initJsonButtons() {
+        saveButton = new JButton("Save your items");
+        saveButton.setActionCommand("save");
+        saveButton.addActionListener(this);
+        loadButton = new JButton("Load previously saved items");
+        loadButton.setActionCommand("load");
+        loadButton.addActionListener(this);
     }
 
     private void initPanels() {
@@ -68,7 +89,7 @@ public class AuctionHouseGUI extends JFrame {
     }
 
     private void storePanels() {
-        mainPanel.setLayout(new GridLayout(6,1));
+        mainPanel.setLayout(new GridLayout(6, 1));
         addPanel.add(new JLabel("Add an item:"));
         addPanel.add(addButton);
         mainPanel.add(addPanel);
@@ -89,5 +110,51 @@ public class AuctionHouseGUI extends JFrame {
         mainPanel.add(runPanel);
     }
 
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()) {
+            case "add":
+                addItem();
+                break;
+            case "remove":
+                removeItem();
+                break;
+            case "view":
+                viewItems();
+                break;
+            case "run":
+                runHouse();
+                break;
+            case "load":
+                loadItems();
+                break;
+            case "save":
+                saveItems();
+                break;
+        }
+    }
+
+    private void addItem() {
+        System.out.println("hi");
+    }
+
+    private void removeItem() {
+        System.out.println("bye");
+    }
+
+    private void viewItems() {
+        System.out.println("a");
+    }
+
+    private void runHouse() {
+        System.out.println("b");
+    }
+
+    private void loadItems() {
+        System.out.println("c");
+    }
+
+    private void saveItems() {
+        System.out.println("d");
+    }
 
 }
